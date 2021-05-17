67444a3a91025805c3d459205bd02c8aa2b2a7ac
mdaazam <52259999+mdaazam@users.noreply.github.com>
2021-05-17 19:23:06 +0530

    azam commit

---
+++ src/modules/SquareComponent.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const SquareComponent = (props) => {
+    const classes = (props.className ? `${props.className} square` : `square`)
+    return (
+        <span
+            className={classes}
+            onClick={() => props.onClick(props.index)}>
+           {props.state}
+        </span>
+    )
+}
+export default SquareComponent
